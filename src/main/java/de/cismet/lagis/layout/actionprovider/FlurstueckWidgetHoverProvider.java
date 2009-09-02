ffc3d6c50b08e5cbdd2e5e5b5d9e5bc81b8435af
Michael Brill <michael.brill@cismet.de>
2009-09-02 14:29:21 +0000

    Animation for hovering a node ! :-D

--- src/main/java/de/cismet/lagis/layout/actionprovider/FlurstueckWidgetHoverProvider.java
+++ src/main/java/de/cismet/lagis/layout/actionprovider/FlurstueckWidgetHoverProvider.java
@@ -5,7 +5,9 @@
 
 package de.cismet.lagis.layout.actionprovider;
 
+import java.awt.Rectangle;
 import org.netbeans.api.visual.action.TwoStateHoverProvider;
+import org.netbeans.api.visual.graph.GraphScene;
 import org.netbeans.api.visual.widget.Widget;
 
 /**
@@ -16,13 +18,21 @@ import org.netbeans.api.visual.widget.Widget;
  */
 public class FlurstueckWidgetHoverProvider implements TwoStateHoverProvider {
 
+    private Rectangle preferredBounds;
+    private GraphScene scene;
+
+    public FlurstueckWidgetHoverProvider(GraphScene scene) {
+        super();
+        this.scene = scene;
+    }
+
     /**
      * Method is called when the mouse cursor exits a FlurstueckHistoryWidget
      * @param w Widget wich was pointed on
      */
     @Override
     public void unsetHovering(Widget w) {
-        System.out.println("Hovering finished" + w);
+        scene.getSceneAnimator().animatePreferredBounds(w, preferredBounds);
     }
 
     /**
@@ -31,7 +41,10 @@ public class FlurstueckWidgetHoverProvider implements TwoStateHoverProvider {
      */
     @Override
     public void setHovering(Widget w) {
-        System.out.println("Hovering Widget" + w);
+        preferredBounds = w.getBounds();
+        Rectangle newBounds = new Rectangle(preferredBounds.x -10, preferredBounds.y -10,
+                preferredBounds.width + 10, preferredBounds.height + 10);
+        scene.getSceneAnimator().animatePreferredBounds(w, newBounds);
     }
 
 }
