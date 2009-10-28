7cd82fa86dfa2f55279e2ff2a4f4ac6275c98e5a
Michael Brill <michael.brill@cismet.de>
2009-10-28 09:00:37 +0000

    [issue:0001205]  Documentation and code cleanup FlurstueckSelectProvider

--- src/main/java/de/cismet/lagis/layout/actionprovider/FlurstueckSelectProvider.java
+++ src/main/java/de/cismet/lagis/layout/actionprovider/FlurstueckSelectProvider.java
@@ -1,71 +1,87 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package de.cismet.lagis.layout.actionprovider;
 
 import de.cismet.lagis.layout.model.HistoryPanelModel;
 import de.cismet.lagis.layout.widget.FlurstueckHistoryWidget;
 import de.cismet.lagis.layout.widget.HighlightWidget;
-import java.awt.Color;
 import java.awt.Point;
 import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeSupport;
-import java.util.Iterator;
 import org.apache.log4j.Logger;
 import org.netbeans.api.visual.action.SelectProvider;
-import org.netbeans.api.visual.border.BorderFactory;
 import org.netbeans.api.visual.widget.Widget;
 
 /**
+ * FlurstueckSelectProvider takes care about selecting flurstueck nodes in the 
+ * NetBeans Visual environment. It causes the scene to display a highlight widget
+ * for the selected node and informs any observer about the selection. Currently
+ * the only observer is the main class of the history module, which on selection displays
+ * additional information about a flurstueck.
  *
  * @author mbrill
  */
 public class FlurstueckSelectProvider implements SelectProvider {
 
+    /**
+     * Scene of the graph. The scene is saved for further reference when setting
+     * and deleting highlight widgets
+     */
     private HistoryPanelModel scene;
     private final Logger log = org.apache.log4j.Logger.getLogger(this.getClass());
+
+    /**
+     * PropertyChangeSupport enables this class to inform observers in form of
+     * PropertyChangeEvents
+     */
     private PropertyChangeSupport propChangeSupport;
 
+    /**
+     * Constructor sets the scene and initialises the PropertyChangeSupport
+     * @param scene
+     */
     public FlurstueckSelectProvider(HistoryPanelModel scene) {
         this.scene = scene;
         propChangeSupport = new PropertyChangeSupport(this);
     }
 
+    /**
+     * Called to check whether aiming is allowe
+     * @param widget the aimed widget
+     * @param localLocation the local location of a mouse cursor while aiming is invoked by an user
+     * @param invertSelection if true, then the invert selection is invoked by an user.
+     * @return false
+     */
     @Override
     public boolean isAimingAllowed(Widget widget, Point localLocation, boolean invertSelection) {
         return false;
     }
 
+    /**
+     * Called to check whether the selection is allowed.
+     * @param widget the selected widget
+     * @param localLocation the local location of a mouse cursor while selection is invoked by an user
+     * @param invertSelection if true, then the invert selection is invoked by an user.
+     * @return true
+     */
     @Override
     public boolean isSelectionAllowed(Widget widget, Point localLocation, boolean invertSelection) {
         return true;
     }
 
+    /**
+     * Called to perform the selection.
+     * @param widget the selected widget
+     * @param localLocation the local location of a mouse cursor while selection is invoked by an user
+     * @param invertSelection if true, then the invert selection is invoked by an user.
+     */
     @Override
     public void select(Widget widget, Point localLocation, boolean invertSelection) {
-
-        
-//        Iterator<Widget> it = scene.getChildren().iterator();
-//        while (it.hasNext()) {
-//            Widget layer = it.next();
-//            Iterator<Widget> ot = layer.getChildren().iterator();
-//            while (ot.hasNext()) {
-//                Widget w = ot.next();
-//                if (w instanceof FlurstueckHistoryWidget) {
-//                    FlurstueckHistoryWidget fhw = (FlurstueckHistoryWidget) w;
-//
-//                    fhw.setBorder(BorderFactory.createEmptyBorder());
-//                }
-//            }
-//        }
 
         scene.getHighlightLayer().removeChildren();
 
         if (widget instanceof FlurstueckHistoryWidget) {
             log.debug("Flurstueck selected");
             FlurstueckHistoryWidget fhw = (FlurstueckHistoryWidget) widget;
-//            fhw.setBorder(BorderFactory.createLineBorder(6, Color.BLUE));
 
             propChangeSupport.firePropertyChange("flurstueck_selected",
                     null, fhw.getFlurstueck());
@@ -82,6 +98,11 @@ public class FlurstueckSelectProvider implements SelectProvider {
 
     }
 
+    /**
+     * Method to attach observers to the PropertyChangeSupport
+     *
+     * @param listener Observer to be added
+     */
     public void addPropertyChangeListener(PropertyChangeListener listener) {
         propChangeSupport.addPropertyChangeListener(listener);
     }
