be953b379e2bf410c50954c6381b23a76c7787cd
Michael Brill <michael.brill@cismet.de>
2009-11-10 14:16:30 +0000

    [issue:0001271] Tooltip Icon entfernt

--- src/main/java/de/cismet/lagis/layout/widget/PureCoolToolTip.java
+++ src/main/java/de/cismet/lagis/layout/widget/PureCoolToolTip.java
@@ -28,8 +28,8 @@ import javax.swing.JToolTip;
  */
 public class PureCoolToolTip extends JToolTip {
 
-    private ResourceBundle icon_bundle = ResourceBundle.getBundle(
-            "de/cismet/lagis/ressource/history/icon", new Locale("de", "DE"));
+//    private ResourceBundle icon_bundle = ResourceBundle.getBundle(
+//            "de/cismet/lagis/ressource/history/icon", new Locale("de", "DE"));
 
     /**
      * Constructs a new instance of JToolTip using PureCoolToolTipUI and a
@@ -37,9 +37,9 @@ public class PureCoolToolTip extends JToolTip {
      */
     public PureCoolToolTip() {
 
-        ImageIcon icon = new ImageIcon(this.getClass().getResource(
-                icon_bundle.getString("icon_tooltip")));
-        setUI(new PureCoolToolTipUI(icon));
+//        ImageIcon icon = new ImageIcon(this.getClass().getResource(
+//                icon_bundle.getString("icon_tooltip")));
+        setUI(new PureCoolToolTipUI(null));
         setOpaque(false);
 
     }
