f7fc98eabced6b08c5017fdb33d1e63211c478d4
Michael Brill <michael.brill@cismet.de>
2009-11-11 17:02:38 +0000

    [issue:0001096] Position und größe des HighlightWidget angepasst, Blur Effekt entfernt. Endgültige Farbe steht zur Diskussion

--- src/main/java/de/cismet/lagis/layout/widget/HighlightWidget.java
+++ src/main/java/de/cismet/lagis/layout/widget/HighlightWidget.java
@@ -7,6 +7,7 @@ import java.awt.Dimension;
 import java.awt.Graphics2D;
 import java.awt.Point;
 import java.awt.Rectangle;
+import java.awt.RenderingHints;
 import java.awt.image.BufferedImage;
 import org.jdesktop.swingx.image.GaussianBlurFilter;
 import org.netbeans.api.visual.widget.Scene;
@@ -21,10 +22,6 @@ import org.netbeans.api.visual.widget.Widget;
  */
 public class HighlightWidget extends Widget {
 
-    /**
-     * The FlurstueckNodePanel Widget that is highlighted
-     */
-    private Widget foregroundWidget;
 
     /**
      * actual width of the HighlightWidget
@@ -39,9 +36,11 @@ public class HighlightWidget extends Widget {
     /**
      * size that will be added to the size of the highlighted widget. If set >= 0
      * the highlight widget seems to shine from behind the foregroundWidget
-     * (otherwise it can not be seen)
+     * (otherwise it can not be seen). This attribute is to read as the number of
+     * pixels, the Highlight widget is created bigger in each direction than the
+     * widget to highlight.
      */
-    private int addSize = 20;
+    private int addSize = 1;
 
     /**
      * Prefered location of an instance of this class. This is
@@ -55,7 +54,7 @@ public class HighlightWidget extends Widget {
     /**
      * Overall color of an instance of this class
      */
-    private Color highlightColor = new Color(5, 5, 255, 80);
+    private Color highlightColor = new Color(0, 0, 255, 255);
 
 
     /**
@@ -69,7 +68,6 @@ public class HighlightWidget extends Widget {
     public HighlightWidget(Scene scene, Widget foregroundWidget) {
         super(scene);
 
-        this.foregroundWidget = foregroundWidget;
         Rectangle baseBounds = foregroundWidget.getBounds();
         Dimension highlightDim = new Dimension(baseBounds.width + (addSize * 2),
                 baseBounds.height + (addSize * 2));
@@ -80,7 +78,7 @@ public class HighlightWidget extends Widget {
 
         prefLocation = foregroundWidget.getPreferredLocation();
         prefLocation.x -= addSize;
-        prefLocation.y -= addSize;
+        prefLocation.y -= addSize + 2;
 
         this.setPreferredLocation(prefLocation);
     }
@@ -98,14 +96,13 @@ public class HighlightWidget extends Widget {
         BufferedImage buffer = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
         Graphics2D bufferGraphics = buffer.createGraphics();
 
+        bufferGraphics.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+                RenderingHints.VALUE_ANTIALIAS_ON);
+
         bufferGraphics.setPaint(highlightColor);
-        bufferGraphics.fillRoundRect(12, 10,
-                width - (addSize + 5), height - (addSize + 5), 30, 30);
+        bufferGraphics.fillRoundRect(0, 0,
+                width, height, 30, 30);
 
-        bufferGraphics.setComposite(new ColorComposite(0.5f));
-
-        GaussianBlurFilter blurFilter = new GaussianBlurFilter(8);
-        buffer = blurFilter.filter(buffer, null);
 
         g2d.drawImage(buffer, 0, 0, null);
 
