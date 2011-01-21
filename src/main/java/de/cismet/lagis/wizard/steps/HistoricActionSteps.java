feca6fe63444c3e8f2bc82f385feeb72ae4a71e0
Thorsten Hell <thorsten.hell@cismet.de>
2011-01-21 17:12:12 +0000

    [issue:1862] setNutzungHistoric jetzt mit Datum

--- src/main/java/de/cismet/lagis/wizard/steps/HistoricActionSteps.java
+++ src/main/java/de/cismet/lagis/wizard/steps/HistoricActionSteps.java
@@ -1,3 +1,10 @@
+/***************************************************
+*
+* cismet GmbH, Saarbruecken, Germany
+*
+*              ... and it just works.
+*
+****************************************************/
 /*
  * HistoricActionSteps.java
  *
@@ -6,20 +13,10 @@
  * To change this template, choose Tools | Template Manager
  * and open the template in the editor.
  */
-
 package de.cismet.lagis.wizard.steps;
 
-import de.cismet.lagis.broker.EJBroker;
-import de.cismet.lagis.broker.LagisBroker;
-import de.cismet.lagis.wizard.panels.HistoricActionPanel;
-import de.cismet.lagisEE.bean.Exception.ActionNotSuccessfulException;
-import de.cismet.lagisEE.entity.core.FlurstueckSchluessel;
-import de.cismet.lagisEE.entity.locking.Sperre;
-import java.awt.EventQueue;
-import java.util.Map;
-import javax.swing.JComponent;
-import javax.swing.JOptionPane;
 import org.apache.log4j.Logger;
+
 import org.netbeans.spi.wizard.DeferredWizardResult;
 import org.netbeans.spi.wizard.ResultProgressHandle;
 import org.netbeans.spi.wizard.Summary;
@@ -27,108 +24,182 @@ import org.netbeans.spi.wizard.WizardController;
 import org.netbeans.spi.wizard.WizardException;
 import org.netbeans.spi.wizard.WizardPanelProvider;
 
+import java.awt.EventQueue;
+
+import java.util.Date;
+import java.util.Map;
+
+import javax.swing.JComponent;
+import javax.swing.JOptionPane;
+
+import de.cismet.lagis.broker.EJBroker;
+import de.cismet.lagis.broker.LagisBroker;
+
+import de.cismet.lagis.wizard.panels.HistoricActionPanel;
+
+import de.cismet.lagisEE.bean.Exception.ActionNotSuccessfulException;
+
+import de.cismet.lagisEE.entity.core.FlurstueckSchluessel;
+import de.cismet.lagisEE.entity.locking.Sperre;
+
 /**
+ * DOCUMENT ME!
  *
- * @author Sebastian Puhl
+ * @author   Sebastian Puhl
+ * @version  $Revision$, $Date$
  */
 public class HistoricActionSteps extends WizardPanelProvider {
-    
-    /** Creates a new instance of HistoricActionSteps */
+
+    //~ Constructors -----------------------------------------------------------
+
+    /**
+     * Creates a new instance of HistoricActionSteps.
+     */
     public HistoricActionSteps() {
-        super("Flurstück historisch setzen...",
-                new String[] { "Flurstück auswählen"},
-                new String[] { "Auswahl des Flurstücks"});
+        super(
+            "Flurstück historisch setzen...",
+            new String[] { "Flurstück auswählen" },
+            new String[] { "Auswahl des Flurstücks" });
     }
-    
-    protected JComponent createPanel(WizardController wizardController, String id, Map wizardData) {
+
+    //~ Methods ----------------------------------------------------------------
+
+    @Override
+    protected JComponent createPanel(final WizardController wizardController, final String id, final Map wizardData) {
         return new HistoricActionPanel(wizardController, wizardData);
     }
-    
-    public boolean cancel(Map settings) {
-        //return true;
-        boolean dialogShouldClose = JOptionPane.showConfirmDialog(LagisBroker.getInstance().getParentComponent(),
+
+    @Override
+    public boolean cancel(final Map settings) {
+        // return true;
+        final boolean dialogShouldClose = JOptionPane.showConfirmDialog(LagisBroker.getInstance().getParentComponent(),
                 "Möchten Sie den Bearbeitungsvorgang beenden?") == JOptionPane.OK_OPTION;
         return dialogShouldClose;
     }
-    
-    protected Object finish(Map settings) throws WizardException {
+
+    @Override
+    protected Object finish(final Map settings) throws WizardException {
         return new BackgroundResultCreator();
     }
-    
-    
-    static class BackgroundResultCreator extends DeferredWizardResult{
+
+    //~ Inner Classes ----------------------------------------------------------
+
+    /**
+     * DOCUMENT ME!
+     *
+     * @version  $Revision$, $Date$
+     */
+    static class BackgroundResultCreator extends DeferredWizardResult {
+
+        //~ Instance fields ----------------------------------------------------
+
         private final Logger log = org.apache.log4j.Logger.getLogger(this.getClass());
-        public void start(Map wizardData, ResultProgressHandle progress) {
-            log.debug("WizardFinisher: Flurstueck historisch setzen: ");
+
+        //~ Methods ------------------------------------------------------------
+
+        @Override
+        public void start(final Map wizardData, final ResultProgressHandle progress) {
+            if (log.isDebugEnabled()) {
+                log.debug("WizardFinisher: Flurstueck historisch setzen: ");
+            }
             assert !EventQueue.isDispatchThread();
-            final FlurstueckSchluessel historicKey = (FlurstueckSchluessel)wizardData.get(HistoricActionPanel.KEY_HISTORIC_CANDIDATE);
-            log.debug("Flurstück das historisch gesetzt werden soll: "+historicKey.getKeyString());
-            Sperre sperre=null;
+            final FlurstueckSchluessel historicKey = (FlurstueckSchluessel)wizardData.get(
+                    HistoricActionPanel.KEY_HISTORIC_CANDIDATE);
+            final Date histDate = (Date)wizardData.get(
+                    HistoricActionPanel.KEY_HISTORIC_DATE);
+            if (log.isDebugEnabled()) {
+                log.debug("Flurstück das historisch gesetzt werden soll: " + historicKey.getKeyString());
+            }
+
+            Sperre sperre = null;
             try {
                 progress.setBusy("Flurstück wird historisch gesetzt");
-                //EJBroker.getInstance().createFlurstueck(key);
-                //HistoricResult result = EJBroker.getInstance().setFlurstueckHistoric(historicKey);
-                //TODO schlechte Postion verwirrt den Benutzer wäre besser wenn sie ganz zum Schluss käme
-                //TODO besser setHistoric mit sperre versehen als immer die sperre vorher zu setzen
-                Sperre other = EJBroker.getInstance().isLocked(historicKey);
-                if(other == null){
-                    sperre = EJBroker.getInstance().createLock(new Sperre(historicKey,LagisBroker.getInstance().getAccountName()));
-                    if(sperre != null){
-                        if(EJBroker.getInstance().setFlurstueckHistoric(historicKey)){
-                            Summary summary;
+                // EJBroker.getInstance().createFlurstueck(key);
+                // HistoricResult result = EJBroker.getInstance().setFlurstueckHistoric(historicKey);
+                // TODO schlechte Postion verwirrt den Benutzer wäre besser wenn sie ganz zum Schluss käme
+                // TODO besser setHistoric mit sperre versehen als immer die sperre vorher zu setzen
+                final Sperre other = EJBroker.getInstance().isLocked(historicKey);
+                if (other == null) {
+                    sperre = EJBroker.getInstance()
+                                .createLock(new Sperre(historicKey, LagisBroker.getInstance().getAccountName()));
+                    if (sperre != null) {
+                        System.out.println("datum:" + histDate);
+                        if (EJBroker.getInstance().setFlurstueckHistoric(historicKey, histDate)) {
+                            final Summary summary;
                             EJBroker.getInstance().releaseLock(sperre);
-                            if(LagisBroker.getInstance().getCurrentFlurstueckSchluessel() != null && FlurstueckSchluessel.FLURSTUECK_EQUALATOR.pedanticEquals(LagisBroker.getInstance().getCurrentFlurstueckSchluessel(),historicKey)){
-                                log.debug("Aktuelles flurstück wurde historisch --> update");
-                                try{
+                            if ((LagisBroker.getInstance().getCurrentFlurstueckSchluessel() != null)
+                                        && FlurstueckSchluessel.FLURSTUECK_EQUALATOR.pedanticEquals(
+                                            LagisBroker.getInstance().getCurrentFlurstueckSchluessel(),
+                                            historicKey)) {
+                                if (log.isDebugEnabled()) {
+                                    log.debug("Aktuelles flurstück wurde historisch --> update");
+                                }
+                                try {
                                     LagisBroker.getInstance().loadFlurstueck(historicKey);
-                                }catch(Exception ex){
-                                    log.debug("Fehler beim updaten/laden der FlurstueckSchluessel/Flurstücks", ex);
+                                } catch (Exception ex) {
+                                    if (log.isDebugEnabled()) {
+                                        log.debug("Fehler beim updaten/laden der FlurstueckSchluessel/Flurstücks", ex);
+                                    }
                                 }
                             } else {
-                                final boolean changeFlurstueck = JOptionPane.showConfirmDialog(LagisBroker.getInstance().getParentComponent(),
-                                        "Möchten Sie zu dem Flurstück wechseln?","Flurstückwechsel",JOptionPane.YES_NO_OPTION) == JOptionPane.YES_OPTION;
+                                final boolean changeFlurstueck = JOptionPane.showConfirmDialog(LagisBroker.getInstance()
+                                                .getParentComponent(),
+                                        "Möchten Sie zu dem Flurstück wechseln?",
+                                        "Flurstückwechsel",
+                                        JOptionPane.YES_NO_OPTION) == JOptionPane.YES_OPTION;
                                 EventQueue.invokeLater(new Runnable() {
-                                    public void run() {
-                                        if(changeFlurstueck){
-                                            LagisBroker.getInstance().loadFlurstueck(historicKey);
-                                        } else {
-                                            LagisBroker.getInstance().reloadFlurstueckKeys();
+
+                                        @Override
+                                        public void run() {
+                                            if (changeFlurstueck) {
+                                                LagisBroker.getInstance().loadFlurstueck(historicKey);
+                                            } else {
+                                                LagisBroker.getInstance().reloadFlurstueckKeys();
+                                            }
                                         }
-                                    }
-                                });
-                            }                            
-                            summary = Summary.create("Flurstück: \n\t"+"\""+historicKey.getKeyString()+"\" \n\nkonnte erfolgreich historisch gesetzt werden",historicKey);
+                                    });
+                            }
+                            summary = Summary.create("Flurstück: \n\t" + "\"" + historicKey.getKeyString()
+                                            + "\" \n\nkonnte erfolgreich historisch gesetzt werden",
+                                    historicKey);
                             progress.finished(summary);
                         } else {
-                            progress.failed("Es war nicht möglich das Flurstück:\n\t\""+historicKey.getKeyString()+"\"\nhistorisch zu setzen, bitte wenden Sie sich an Ihren Systemadministrator",false);
+                            progress.failed("Es war nicht möglich das Flurstück:\n\t\"" + historicKey.getKeyString()
+                                        + "\"\nhistorisch zu setzen, bitte wenden Sie sich an Ihren Systemadministrator",
+                                false);
                         }
                     } else {
-                        progress.failed("Es war nicht möglich das Flurstück:\n\t\""+historicKey.getKeyString()+"\"\nhistorisch zu setzen, es konnte keine Sperre angelegt werden.",false);
+                        progress.failed("Es war nicht möglich das Flurstück:\n\t\"" + historicKey.getKeyString()
+                                    + "\"\nhistorisch zu setzen, es konnte keine Sperre angelegt werden.",
+                            false);
                     }
                 } else {
-                    progress.failed("Es war nicht möglich das Flurstück:\n\t\""+historicKey.getKeyString()+"\"\nhistorisch zu setzen, es ist von einem anderen Benutzer gesperrt: "+other.getBenutzerkonto(),false);
+                    progress.failed("Es war nicht möglich das Flurstück:\n\t\"" + historicKey.getKeyString()
+                                + "\"\nhistorisch zu setzen, es ist von einem anderen Benutzer gesperrt: "
+                                + other.getBenutzerkonto(),
+                        false);
                 }
             } catch (Exception e) {
-                log.error("Fehler beim historischsetzen eines Flurstücks: ",e);
-                try{
+                log.error("Fehler beim historischsetzen eines Flurstücks: ", e);
+                try {
                     EJBroker.getInstance().releaseLock(sperre);
-                }catch(Exception ex){
-                    log.error("Fehler beim lösen der Sperre",ex);
+                } catch (Exception ex) {
+                    log.error("Fehler beim lösen der Sperre", ex);
                 }
-                final StringBuffer buffer = new StringBuffer("Es war nicht möglich das Flurstück:\n\t\""+historicKey.getKeyString()+"\"\nhistorisch zu setzen bzw. zu löschen. Fehler:\n");
-                if(e instanceof ActionNotSuccessfulException){
-                    ActionNotSuccessfulException reason = (ActionNotSuccessfulException) e;
-                    if(reason.hasNestedExceptions()){
-                        log.error("Nested setFlurstueckHistoric Exceptions: ",reason.getNestedExceptions());
+                final StringBuffer buffer = new StringBuffer("Es war nicht möglich das Flurstück:\n\t\""
+                                + historicKey.getKeyString() + "\"\nhistorisch zu setzen bzw. zu löschen. Fehler:\n");
+                if (e instanceof ActionNotSuccessfulException) {
+                    final ActionNotSuccessfulException reason = (ActionNotSuccessfulException)e;
+                    if (reason.hasNestedExceptions()) {
+                        log.error("Nested setFlurstueckHistoric Exceptions: ", reason.getNestedExceptions());
                     }
                     buffer.append(reason.getMessage());
                 } else {
-                    log.error("Unbekannter Fehler: ",e);
+                    log.error("Unbekannter Fehler: ", e);
                     buffer.append("Unbekannter Fehler bitte wenden Sie sich an Ihren Systemadministrator");
                 }
-                progress.failed(buffer.toString(),false);
+                progress.failed(buffer.toString(), false);
             }
         }
     }
-    
 }
