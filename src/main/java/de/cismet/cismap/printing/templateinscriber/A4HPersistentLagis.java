8bed78962877f32ff6974e140b27799f3fdf0165
Gilles Baatz <gilles.baatz@cismet.de>
2013-04-29 17:31:12 +0200

    #9 added the class A4HPersistentLagis, which inherits from A4HPersistent and only changes its label texts

---
+++ src/main/java/de/cismet/cismap/printing/templateinscriber/A4HPersistentLagis.java
@@ -0,0 +1,28 @@
+/***************************************************
+*
+* cismet GmbH, Saarbruecken, Germany
+*
+*              ... and it just works.
+*
+****************************************************/
+package de.cismet.cismap.printing.templateinscriber;
+
+/**
+ * DOCUMENT ME!
+ *
+ * @author   gbaatz
+ * @version  $Revision$, $Date$
+ */
+public class A4HPersistentLagis extends A4HPersistent {
+
+    //~ Constructors -----------------------------------------------------------
+
+    /**
+     * Creates a new A4HPersistentLagis object.
+     */
+    public A4HPersistentLagis() {
+        super();
+        setTextOfJLabel1("Zeile 1");
+        setTextOfJLabel2("Zeile 2");
+    }
+}
