0e8499431ae336db359af4befc17a6a5eea701b0
Gilles Baatz <gilles.baatz@cismet.de>
2013-05-23 09:27:49 +0200

    #10 made the Logger static in AbstractCidsBeanTable_Lagis

--- src/main/java/de/cismet/lagis/gui/tables/AbstractCidsBeanTable_Lagis.java
+++ src/main/java/de/cismet/lagis/gui/tables/AbstractCidsBeanTable_Lagis.java
@@ -1,13 +1,13 @@
-/**
- * *************************************************
- *
- * cismet GmbH, Saarbruecken, Germany
- * 
-* ... and it just works.
- * 
-***************************************************
- */
+/***************************************************
+*
+* cismet GmbH, Saarbruecken, Germany
+*
+*              ... and it just works.
+*
+****************************************************/
 package de.cismet.lagis.gui.tables;
+
+import org.apache.log4j.Logger;
 
 import org.jdesktop.swingx.JXTable;
 import org.jdesktop.swingx.decorator.SortOrder;
@@ -29,23 +29,27 @@ import javax.swing.table.TableModel;
 import de.cismet.cids.dynamics.CidsBean;
 
 import de.cismet.lagis.models.CidsBeanTableModel_Lagis;
-import org.apache.log4j.Logger;
 
 /**
  * DOCUMENT ME!
  *
- * @author gbaatz
- * @version $Revision$, $Date$
+ * @author   gbaatz
+ * @version  $Revision$, $Date$
  */
 public abstract class AbstractCidsBeanTable_Lagis extends JXTable {
 
+    //~ Static fields/initializers ---------------------------------------------
+
+    private static final Logger LOG = org.apache.log4j.Logger.getLogger(AbstractCidsBeanTable_Lagis.class);
+
     //~ Instance fields --------------------------------------------------------
-    private final Logger log = org.apache.log4j.Logger.getLogger(this.getClass());
+
     private int previously_sorted_column_index = 0;
     private SortOrder previously_used_sort_order = SortOrder.ASCENDING;
     private JToggleButton tbtnSort;
 
     //~ Constructors -----------------------------------------------------------
+
     /**
      * Creates a new AbstractCidsBeanTable_Lagis object.
      */
@@ -54,76 +58,81 @@ public abstract class AbstractCidsBeanTable_Lagis extends JXTable {
     }
 
     //~ Methods ----------------------------------------------------------------
+
     /**
      * DOCUMENT ME!
      *
-     * @return DOCUMENT ME!
+     * @return  DOCUMENT ME!
      */
     public Action getAddAction() {
         return new AbstractAction() {
-            @Override
-            public void actionPerformed(final ActionEvent e) {
-                btnAddActionPerformed(e);
-            }
-        };
+
+                @Override
+                public void actionPerformed(final ActionEvent e) {
+                    btnAddActionPerformed(e);
+                }
+            };
     }
 
     /**
      * DOCUMENT ME!
      *
-     * @return DOCUMENT ME!
+     * @return  DOCUMENT ME!
      */
     public Action getRemoveAction() {
         return new AbstractAction() {
-            @Override
-            public void actionPerformed(final ActionEvent e) {
-                btnRemoveActionPerformed(e);
-            }
-        };
+
+                @Override
+                public void actionPerformed(final ActionEvent e) {
+                    btnRemoveActionPerformed(e);
+                }
+            };
     }
 
     /**
      * DOCUMENT ME!
      *
-     * @return DOCUMENT ME!
+     * @return  DOCUMENT ME!
      */
     public ItemListener getSortItemListener() {
         return new ItemListener() {
-            @Override
-            public void itemStateChanged(final ItemEvent evt) {
-                tbtnSortItemStateChanged(evt);
-            }
-        };
+
+                @Override
+                public void itemStateChanged(final ItemEvent evt) {
+                    tbtnSortItemStateChanged(evt);
+                }
+            };
     }
 
     /**
      * DOCUMENT ME!
      *
-     * @param evt DOCUMENT ME!
+     * @param  evt  DOCUMENT ME!
      */
     protected void btnAddActionPerformed(final ActionEvent evt) {
         getSortButton().setSelected(true);
         this.setSortable(false);
         addNewItem();
         SwingUtilities.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                final AbstractCidsBeanTable_Lagis table = AbstractCidsBeanTable_Lagis.this;
-                table.setRowSelectionInterval(table.getRowCount() - 1, table.getRowCount() - 1);
-                table.scrollRectToVisible(table.getCellRect(table.getRowCount() - 1, 0, true));
-            }
-        });
+
+                @Override
+                public void run() {
+                    final AbstractCidsBeanTable_Lagis table = AbstractCidsBeanTable_Lagis.this;
+                    table.setRowSelectionInterval(table.getRowCount() - 1, table.getRowCount() - 1);
+                    table.scrollRectToVisible(table.getCellRect(table.getRowCount() - 1, 0, true));
+                }
+            });
     }
 
     /**
-     * This method gets called in btnAddActionPerformed()
+     * This method gets called in btnAddActionPerformed().
      */
     protected abstract void addNewItem();
 
     /**
      * DOCUMENT ME!
      *
-     * @param evt DOCUMENT ME!
+     * @param  evt  DOCUMENT ME!
      */
     protected void btnRemoveActionPerformed(final ActionEvent evt) {
         final int currentRow = this.getSelectedRow();
@@ -133,26 +142,25 @@ public abstract class AbstractCidsBeanTable_Lagis extends JXTable {
     }
 
     /**
-     * This method gets called in btnRemoveActionPerformed(). Row is the number
-     * of the row, which should be removed.
+     * This method gets called in btnRemoveActionPerformed(). Row is the number of the row, which should be removed.
      *
-     * @param row DOCUMENT ME!
+     * @param  row  DOCUMENT ME!
      */
     protected abstract void removeItem(int row);
 
     /**
      * DOCUMENT ME!
      *
-     * @param evt DOCUMENT ME!
+     * @param  evt  DOCUMENT ME!
      */
     protected void tbtnSortItemStateChanged(final ItemEvent evt) {
         if (tbtnSort.isSelected()) { // disable sort
-            previously_sorted_column_index = ((JXTable) this).getSortedColumn().getModelIndex();
-            previously_used_sort_order = ((JXTable) this).getSortOrder(previously_sorted_column_index);
-            ((JXTable) this).setSortable(false);
+            previously_sorted_column_index = ((JXTable)this).getSortedColumn().getModelIndex();
+            previously_used_sort_order = ((JXTable)this).getSortOrder(previously_sorted_column_index);
+            ((JXTable)this).setSortable(false);
         } else {                     // sort the table
-            ((JXTable) this).setSortable(true);
-            ((JXTable) this).setSortOrder(previously_sorted_column_index, previously_used_sort_order);
+            ((JXTable)this).setSortable(true);
+            ((JXTable)this).setSortOrder(previously_sorted_column_index, previously_used_sort_order);
         }
         this.scrollRectToVisible(this.getCellRect(this.getSelectedRow(), 0, true));
     }
@@ -160,7 +168,7 @@ public abstract class AbstractCidsBeanTable_Lagis extends JXTable {
     /**
      * DOCUMENT ME!
      *
-     * @param sortButton DOCUMENT ME!
+     * @param  sortButton  DOCUMENT ME!
      */
     public void setSortButton(final JToggleButton sortButton) {
         tbtnSort = sortButton;
@@ -169,27 +177,26 @@ public abstract class AbstractCidsBeanTable_Lagis extends JXTable {
     /**
      * DOCUMENT ME!
      *
-     * @return DOCUMENT ME!
+     * @return  DOCUMENT ME!
      */
     public JToggleButton getSortButton() {
         return tbtnSort;
     }
 
     /**
-     * Additional functionality for CidsBeanTableModel_Lagis models. If newModel
-     * is a subclass of CidsBeanTableModel_Lagis, then this table is added to
-     * the newModel.
+     * Additional functionality for CidsBeanTableModel_Lagis models. If newModel is a subclass of
+     * CidsBeanTableModel_Lagis, then this table is added to the newModel.
      *
-     * @return DOCUMENT ME!
+     * @param  newModel  DOCUMENT ME!
      */
     @Override
     public void setModel(final TableModel newModel) {
         super.setModel(newModel);
         if (newModel instanceof CidsBeanTableModel_Lagis) {
-            ((CidsBeanTableModel_Lagis) newModel).setTable(this);
+            ((CidsBeanTableModel_Lagis)newModel).setTable(this);
         }
-        if (log.isDebugEnabled()) {
-            log.debug("Table was also added to model.");
+        if (LOG.isDebugEnabled()) {
+            LOG.debug("Table was also added to model.");
         }
     }
 }
