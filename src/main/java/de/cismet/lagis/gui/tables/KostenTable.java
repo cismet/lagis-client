f88405a02cfb2f57b20e71ba2b5d2215c96af1b4
Gilles Baatz <gilles.baatz@cismet.de>
2013-05-23 11:24:20 +0200

    #10 created class KostenTable

---
+++ src/main/java/de/cismet/lagis/gui/tables/KostenTable.java
@@ -0,0 +1,54 @@
+/***************************************************
+*
+* cismet GmbH, Saarbruecken, Germany
+*
+*              ... and it just works.
+*
+****************************************************/
+package de.cismet.lagis.gui.tables;
+
+import de.cismet.lagis.models.documents.VertragDocumentModelContainer;
+
+/**
+ * DOCUMENT ME!
+ *
+ * @author   gbaatz
+ * @version  $Revision$, $Date$
+ */
+public class KostenTable extends AbstractCidsBeanTable_Lagis {
+
+    //~ Instance fields --------------------------------------------------------
+
+    private VertragDocumentModelContainer documentContainer;
+
+    //~ Methods ----------------------------------------------------------------
+
+    /**
+     * DOCUMENT ME!
+     *
+     * @return  DOCUMENT ME!
+     */
+    public VertragDocumentModelContainer getDocumentContainer() {
+        return documentContainer;
+    }
+
+    /**
+     * DOCUMENT ME!
+     *
+     * @param  documentContainer  DOCUMENT ME!
+     */
+    public void setDocumentContainer(final VertragDocumentModelContainer documentContainer) {
+        this.documentContainer = documentContainer;
+    }
+
+    @Override
+    protected void addNewItem() {
+        documentContainer.addNewKosten();
+    }
+
+    @Override
+    protected void removeItem(final int row) {
+        documentContainer.removeKosten(this.getFilters().convertRowIndexToModel(
+                row));
+    }
+}
