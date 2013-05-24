39e5184ccade693ae8720fe6598132a717227dad
Gilles Baatz <gilles.baatz@cismet.de>
2013-05-24 15:24:50 +0200

    #10 created NKFOverviewTable

---
+++ src/main/java/de/cismet/lagis/gui/tables/NKFOverviewTable.java
@@ -0,0 +1,29 @@
+/***************************************************
+*
+* cismet GmbH, Saarbruecken, Germany
+*
+*              ... and it just works.
+*
+****************************************************/
+package de.cismet.lagis.gui.tables;
+
+/**
+ * DOCUMENT ME!
+ *
+ * @author   gbaatz
+ * @version  $Revision$, $Date$
+ */
+public class NKFOverviewTable extends AbstractCidsBeanTable_Lagis {
+
+    //~ Methods ----------------------------------------------------------------
+
+    @Override
+    protected void addNewItem() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    protected void removeItem(final int row) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+}
