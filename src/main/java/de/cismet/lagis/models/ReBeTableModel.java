302c5350626c2d038dcf51f92eb0b9f99169a65d
Gilles Baatz <gilles.baatz@cismet.de>
2013-05-27 09:59:39 +0200

    #10 added fireTableDataChanged() to removeCidsBean in ReBeTableModel

--- src/main/java/de/cismet/lagis/models/ReBeTableModel.java
+++ src/main/java/de/cismet/lagis/models/ReBeTableModel.java
@@ -146,6 +146,7 @@ public class ReBeTableModel extends CidsBeanTableModel_Lagis {
             // TODO: Benni: remove CidsBean.delete() call, if removal from ObservedList is sufficient
             reBe.delete();
             getCidsBeans().remove(rowIndex);
+            fireTableDataChanged();
         } catch (final Exception ex) {
             LOG.error("An error occurred while removing ReBe from RebeTableModel", ex);
         }
