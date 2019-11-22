ff555d3a86b63878d9bb08db6c9883c57806ce16
Jean-Michel Ruiz <jean.ruiz@cismet.de>
2019-11-22 18:29:24 +0100

    minor jalopy changes

--- src/main/java/de/cismet/lagis/test/TestOjectsWithGeomToJson.java
+++ src/main/java/de/cismet/lagis/test/TestOjectsWithGeomToJson.java
@@ -1,3 +1,10 @@
+/***************************************************
+*
+* cismet GmbH, Saarbruecken, Germany
+*
+*              ... and it just works.
+*
+****************************************************/
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -7,37 +14,57 @@ package de.cismet.lagis.test;
 
 import Sirius.server.middleware.types.MetaClass;
 import Sirius.server.middleware.types.MetaObject;
+
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.module.SimpleModule;
+
 import com.vividsolutions.jts.geom.Geometry;
-import de.cismet.cids.client.tools.DevelopmentTools;
-import de.cismet.cids.custom.beans.lagis.FlurstueckCustomBean;
-import de.cismet.cids.custom.beans.lagis.FlurstueckSchluesselCustomBean;
-import de.cismet.cids.custom.beans.lagis.MipaCustomBean;
-import de.cismet.cids.custom.beans.lagis.RebeCustomBean;
-import de.cismet.cids.dynamics.CidsBean;
-import de.cismet.connectioncontext.ConnectionContext;
-import de.cismet.lagis.broker.CidsBroker;
-import de.cismet.lagis.broker.LagisBroker;
-import de.cismet.lagis.commons.LagisConstants;
-import de.cismet.lagis.commons.LagisMetaclassConstants;
+
+import org.apache.commons.io.IOUtils;
+
 import java.io.FileOutputStream;
 import java.io.OutputStream;
+
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
-import org.apache.commons.io.IOUtils;
+
+import de.cismet.cids.client.tools.DevelopmentTools;
+
+import de.cismet.cids.custom.beans.lagis.FlurstueckCustomBean;
+import de.cismet.cids.custom.beans.lagis.FlurstueckSchluesselCustomBean;
+import de.cismet.cids.custom.beans.lagis.MipaCustomBean;
+import de.cismet.cids.custom.beans.lagis.RebeCustomBean;
+
+import de.cismet.cids.dynamics.CidsBean;
+
+import de.cismet.connectioncontext.ConnectionContext;
+
+import de.cismet.lagis.broker.CidsBroker;
+import de.cismet.lagis.broker.LagisBroker;
+
+import de.cismet.lagis.commons.LagisConstants;
+import de.cismet.lagis.commons.LagisMetaclassConstants;
 
 /**
+ * DOCUMENT ME!
  *
- * @author jruiz
+ * @author   jruiz
+ * @version  $Revision$, $Date$
  */
 public class TestOjectsWithGeomToJson {
 
-    public static void main(String[] args) {
+    //~ Methods ----------------------------------------------------------------
+
+    /**
+     * DOCUMENT ME!
+     *
+     * @param  args  DOCUMENT ME!
+     */
+    public static void main(final String[] args) {
         try {
             final ObjectMapper mapper = new ObjectMapper();
             final SimpleModule module = new SimpleModule();
@@ -47,65 +74,78 @@ public class TestOjectsWithGeomToJson {
 
             final ConnectionContext context = ConnectionContext.createDummy();
 
-            DevelopmentTools.showSimpleLoginDialog("http://localhost:9986/callserver/binary", LagisConstants.DOMAIN_LAGIS, true, context);
+            DevelopmentTools.showSimpleLoginDialog(
+                "http://localhost:9986/callserver/binary",
+                LagisConstants.DOMAIN_LAGIS,
+                true,
+                context);
 
             LagisBroker.getInstance().setDomain(LagisConstants.DOMAIN_LAGIS);
 
-            final MetaClass mcFlurstueck = CidsBroker.getInstance().getLagisMetaClass(LagisMetaclassConstants.FLURSTUECK);
+            final MetaClass mcFlurstueck = CidsBroker.getInstance()
+                        .getLagisMetaClass(LagisMetaclassConstants.FLURSTUECK);
             final MetaClass mcAlkisFlurstueck = CidsBroker.getInstance().getLagisMetaClass("ALKIS_FLURSTUECK");
 
-            //final List<Integer> multiVerwaltungsBereichFlurstueckIds = Arrays.asList(/*17778,20236,13469,18299,21637,23285,18082,22342,10943,2589,5819,22343,22303,18829,12289,20777,18506,18684,13116,10469,2936,2581,2633,22479,20702,19090,13051,18557,21707,8632,22139,22341,20504,1190,5718,9179,2941,2197,22728,2506,22654,18897,20779,17740,12479,21555,21329,21976,22796,9737,19766,21550,1137,22983,22299,4526,5381,5471,21818,21786,6704,21439,21253,12286,21138,12444,17649,18388,9841,19706,20607,21369,4416,8633,22138,8391,6952,8001,11612,11490,990,1166,1184,6182,9446,2126,9240,21133,7923,21456,*/13123/*,12445*/);
+            // final List<Integer> multiVerwaltungsBereichFlurstueckIds =
+            // Arrays.asList(/*17778,20236,13469,18299,21637,23285,18082,22342,10943,2589,5819,22343,22303,18829,12289,20777,18506,18684,13116,10469,2936,2581,2633,22479,20702,19090,13051,18557,21707,8632,22139,22341,20504,1190,5718,9179,2941,2197,22728,2506,22654,18897,20779,17740,12479,21555,21329,21976,22796,9737,19766,21550,1137,22983,22299,4526,5381,5471,21818,21786,6704,21439,21253,12286,21138,12444,17649,18388,9841,19706,20607,21369,4416,8633,22138,8391,6952,8001,11612,11490,990,1166,1184,6182,9446,2126,9240,21133,7923,21456,*/13123/*,12445*/);
             final List<Integer> multiVerwaltungsBereichFlurstueckIds = Arrays.asList(22726);
 
-            for (final Integer flurstueckId : multiVerwaltungsBereichFlurstueckIds) {                        
-                final FlurstueckCustomBean flurstueck = (FlurstueckCustomBean)CidsBroker.getInstance().getMetaObject(flurstueckId, mcFlurstueck.getId(), LagisConstants.DOMAIN_LAGIS).getBean();
+            for (final Integer flurstueckId : multiVerwaltungsBereichFlurstueckIds) {
+                final FlurstueckCustomBean flurstueck = (FlurstueckCustomBean)CidsBroker.getInstance()
+                            .getMetaObject(flurstueckId, mcFlurstueck.getId(), LagisConstants.DOMAIN_LAGIS)
+                            .getBean();
                 final FlurstueckSchluesselCustomBean flurstueckSchluessel = flurstueck.getFlurstueckSchluessel();
 
-                final String alkisFlurstueckQuery = String.format("SELECT %d, id FROM alkis_flurstueck WHERE fk_schluessel = %d LIMIT 1;",
+                final String alkisFlurstueckQuery = String.format(
+                        "SELECT %d, id FROM alkis_flurstueck WHERE fk_schluessel = %d LIMIT 1;",
                         mcAlkisFlurstueck.getID(),
-                        flurstueckSchluessel.getId()
-                    );
+                        flurstueckSchluessel.getId());
 
-                final MetaObject[] mosAlkisFlurstueck = CidsBroker.getInstance().getLagisMetaObject(alkisFlurstueckQuery);
-                final CidsBean alkisFlurstueck = mosAlkisFlurstueck != null && mosAlkisFlurstueck.length > 0 ? 
-                        mosAlkisFlurstueck[0].getBean() 
-                        : null;
+                final MetaObject[] mosAlkisFlurstueck = CidsBroker.getInstance()
+                            .getLagisMetaObject(alkisFlurstueckQuery);
+                final CidsBean alkisFlurstueck = ((mosAlkisFlurstueck != null) && (mosAlkisFlurstueck.length > 0))
+                    ? mosAlkisFlurstueck[0].getBean() : null;
                 final Geometry flurstueckGeometrie = (Geometry)alkisFlurstueck.getProperty("geometrie");
 
                 final Collection<MipaCustomBean> mipas = LagisBroker.getInstance().getMiPas(flurstueckGeometrie);
-                final Collection<RebeCustomBean> rebes = LagisBroker.getInstance().getRechteUndBelastungen(flurstueckGeometrie);
+                final Collection<RebeCustomBean> rebes = LagisBroker.getInstance()
+                            .getRechteUndBelastungen(flurstueckGeometrie);
 
-                final String historyDot = LagisBroker.getInstance().getHistoryGraph(flurstueck, 
-                                    LagisBroker.HistoryLevel.DIRECT_RELATIONS,
-                                    0,
-                                    LagisBroker.HistorySibblingLevel.NONE,
-                                    0,
-                                    LagisBroker.HistoryType.BOTH,
-                                    null);
+                final String historyDot = LagisBroker.getInstance()
+                            .getHistoryGraph(
+                                flurstueck,
+                                LagisBroker.HistoryLevel.DIRECT_RELATIONS,
+                                0,
+                                LagisBroker.HistorySibblingLevel.NONE,
+                                0,
+                                LagisBroker.HistoryType.BOTH,
+                                null);
 
-                final String flurstueckJson = flurstueck.toJSONString(false);            
+                final String flurstueckJson = flurstueck.toJSONString(false);
                 final String mipasJson = CidsBean.toJSONString(false, (Collection)mipas);
                 final String rebesJson = CidsBean.toJSONString(false, (Collection)rebes);
 
                 final Map<String, Object> flurstueckMap = mapper.readValue(
-                        flurstueckJson, 
-                        new TypeReference<Map<String, Object>>() {}
-                );
+                        flurstueckJson,
+                        new TypeReference<Map<String, Object>>() {
+                        });
+
                 final List<Map> mipasMap = mapper.readValue(
-                        mipasJson, 
-                        new TypeReference<List<Map>>() {}
-                );
+                        mipasJson,
+                        new TypeReference<List<Map>>() {
+                        });
+
                 final List<Map> rebesMap = mapper.readValue(
-                        rebesJson, 
-                        new TypeReference<List<Map>>() {}
-                );
+                        rebesJson,
+                        new TypeReference<List<Map>>() {
+                        });
 
                 flurstueckMap.put("historyDot", historyDot);
                 flurstueckMap.put("mipas", mipasMap);
                 flurstueckMap.put("rebes", rebesMap);
 
                 final String prettyJson = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(flurstueckMap);
-                try (final OutputStream out = new FileOutputStream("/tmp/fs_" + flurstueckId + ".json")) {
+                try(final OutputStream out = new FileOutputStream("/tmp/fs_" + flurstueckId + ".json")) {
                     IOUtils.write(prettyJson, out, "UTF-8");
                 }
                 System.exit(0);
@@ -118,26 +158,26 @@ public class TestOjectsWithGeomToJson {
 }
 
 /* -- flurstuecke mit mehreren verwaltungsbereichen
-select 
-	sub.flurstueck_id,
-	gemarkung.bezeichnung || ' ' 
-	|| flurstueck_schluessel.flur || '-' 
-	|| flurstueck_schluessel.flurstueck_zaehler || '/' 
-	|| flurstueck_schluessel.flurstueck_nenner,
-	sub.anzahl_verwaltungsbereiche
+select
+        sub.flurstueck_id,
+        gemarkung.bezeichnung || ' '
+        || flurstueck_schluessel.flur || '-'
+        || flurstueck_schluessel.flurstueck_zaehler || '/'
+        || flurstueck_schluessel.flurstueck_nenner,
+        sub.anzahl_verwaltungsbereiche
 from flurstueck_schluessel
 left join gemarkung on flurstueck_schluessel.fk_gemarkung = gemarkung.id
 left join (
-	select 
-		flurstueck.id as flurstueck_id, 
-		flurstueck.fk_flurstueck_schluessel as flurstueck_schluessel_id, 
-		count(*) as anzahl_verwaltungsbereiche
-	from flurstueck_schluessel 
-	left join flurstueck on flurstueck.fk_flurstueck_schluessel = flurstueck_schluessel.id
-	left join verwaltungsbereiche_eintrag on flurstueck.id = verwaltungsbereiche_eintrag.fk_flurstueck
-	left join verwaltungsbereich on verwaltungsbereiche_eintrag.id = verwaltungsbereich.fk_verwaltungsbereiche_eintrag
-	group by flurstueck.id, flurstueck.fk_flurstueck_schluessel
-	having count(*) > 4
+        select
+                flurstueck.id as flurstueck_id,
+                flurstueck.fk_flurstueck_schluessel as flurstueck_schluessel_id,
+                count(*) as anzahl_verwaltungsbereiche
+        from flurstueck_schluessel
+        left join flurstueck on flurstueck.fk_flurstueck_schluessel = flurstueck_schluessel.id
+        left join verwaltungsbereiche_eintrag on flurstueck.id = verwaltungsbereiche_eintrag.fk_flurstueck
+        left join verwaltungsbereich on verwaltungsbereiche_eintrag.id = verwaltungsbereich.fk_verwaltungsbereiche_eintrag
+        group by flurstueck.id, flurstueck.fk_flurstueck_schluessel
+        having count(*) > 4
 ) as sub on sub.flurstueck_schluessel_id = flurstueck_schluessel.id
 where sub.anzahl_verwaltungsbereiche > 0
 order by sub.anzahl_verwaltungsbereiche desc;
@@ -237,29 +277,29 @@ order by sub.anzahl_verwaltungsbereiche desc;
 */
 
 /* -- flurstuecke mit mehreren mipa
-select 
-	sub.flurstueck_id,
-	gemarkung.bezeichnung || ' ' 
-	|| flurstueck_schluessel.flur || '-' 
-	|| flurstueck_schluessel.flurstueck_zaehler || '/' 
-	|| flurstueck_schluessel.flurstueck_nenner,
-	sub.anzahl_mipa
+select
+        sub.flurstueck_id,
+        gemarkung.bezeichnung || ' '
+        || flurstueck_schluessel.flur || '-'
+        || flurstueck_schluessel.flurstueck_zaehler || '/'
+        || flurstueck_schluessel.flurstueck_nenner,
+        sub.anzahl_mipa
 from flurstueck_schluessel
 left join gemarkung on flurstueck_schluessel.fk_gemarkung = gemarkung.id
 left join (
-	select 
-		flurstueck.id as flurstueck_id, 
-		alkis_flurstueck.fk_schluessel as flurstueck_schluessel_id, 
-		count(*) as anzahl_mipa
-	from 
-		flurstueck left join flurstueck_schluessel on flurstueck.fk_flurstueck_schluessel = flurstueck_schluessel.id
-		left join alkis_flurstueck on alkis_flurstueck.fk_schluessel = flurstueck_schluessel.id,		
-		mipa left join geom as mipa_geom on mipa.fk_geom = mipa_geom.id
-	where 
-		st_area(alkis_flurstueck.geometrie) > 100000 AND
-		st_intersects(mipa_geom.geo_field, st_buffer(alkis_flurstueck.geometrie, -1)) 
-	group by flurstueck.id, alkis_flurstueck.fk_schluessel
-	having count(*) > 4
+        select
+                flurstueck.id as flurstueck_id,
+                alkis_flurstueck.fk_schluessel as flurstueck_schluessel_id,
+                count(*) as anzahl_mipa
+        from
+                flurstueck left join flurstueck_schluessel on flurstueck.fk_flurstueck_schluessel = flurstueck_schluessel.id
+                left join alkis_flurstueck on alkis_flurstueck.fk_schluessel = flurstueck_schluessel.id,
+                mipa left join geom as mipa_geom on mipa.fk_geom = mipa_geom.id
+        where
+                st_area(alkis_flurstueck.geometrie) > 100000 AND
+                st_intersects(mipa_geom.geo_field, st_buffer(alkis_flurstueck.geometrie, -1))
+        group by flurstueck.id, alkis_flurstueck.fk_schluessel
+        having count(*) > 4
 ) as sub on sub.flurstueck_schluessel_id = flurstueck_schluessel.id
 where sub.anzahl_mipa > 0
 order by sub.anzahl_mipa desc;
@@ -273,29 +313,29 @@ order by sub.anzahl_mipa desc;
 */
 
 /*
-select 
-	sub.flurstueck_id,
-	gemarkung.bezeichnung || ' ' 
-	|| flurstueck_schluessel.flur || '-' 
-	|| flurstueck_schluessel.flurstueck_zaehler || '/' 
-	|| flurstueck_schluessel.flurstueck_nenner,
-	sub.anzahl_rebe
+select
+        sub.flurstueck_id,
+        gemarkung.bezeichnung || ' '
+        || flurstueck_schluessel.flur || '-'
+        || flurstueck_schluessel.flurstueck_zaehler || '/'
+        || flurstueck_schluessel.flurstueck_nenner,
+        sub.anzahl_rebe
 from flurstueck_schluessel
 left join gemarkung on flurstueck_schluessel.fk_gemarkung = gemarkung.id
 left join (
-	select 
-		flurstueck.id as flurstueck_id, 
-		alkis_flurstueck.fk_schluessel as flurstueck_schluessel_id, 
-		count(*) as anzahl_rebe
-	from 
-		flurstueck left join flurstueck_schluessel on flurstueck.fk_flurstueck_schluessel = flurstueck_schluessel.id
-		left join alkis_flurstueck on alkis_flurstueck.fk_schluessel = flurstueck_schluessel.id,		
-		rebe left join geom as rebe_geom on rebe.fk_geom = rebe_geom.id
-	where 
-		st_area(alkis_flurstueck.geometrie) > 100000 AND
-		st_intersects(rebe_geom.geo_field, st_buffer(alkis_flurstueck.geometrie, -1)) 
-	group by flurstueck.id, alkis_flurstueck.fk_schluessel
-	having count(*) > 4
+        select
+                flurstueck.id as flurstueck_id,
+                alkis_flurstueck.fk_schluessel as flurstueck_schluessel_id,
+                count(*) as anzahl_rebe
+        from
+                flurstueck left join flurstueck_schluessel on flurstueck.fk_flurstueck_schluessel = flurstueck_schluessel.id
+                left join alkis_flurstueck on alkis_flurstueck.fk_schluessel = flurstueck_schluessel.id,
+                rebe left join geom as rebe_geom on rebe.fk_geom = rebe_geom.id
+        where
+                st_area(alkis_flurstueck.geometrie) > 100000 AND
+                st_intersects(rebe_geom.geo_field, st_buffer(alkis_flurstueck.geometrie, -1))
+        group by flurstueck.id, alkis_flurstueck.fk_schluessel
+        having count(*) > 4
 ) as sub on sub.flurstueck_schluessel_id = flurstueck_schluessel.id
 where sub.anzahl_rebe > 0
 order by sub.anzahl_rebe desc;
